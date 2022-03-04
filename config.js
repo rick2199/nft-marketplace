cb08f34f18681610fcdf1e97c739337b0d33864b
Gabriel Rojas <gabriel.rojasa21@gmail.com>
2022-03-04 13:08:05 -0500

    feat: add types

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-
-  export const nftMarketAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-  export const nftAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
-  
+export const nftMarketAddress = "0x4DC0aC3cFB4fd41fb7323a6ef377CF7568D3311a";
+export const nftAddress = "0x27c15394CeA03F2727983c7aB5F5f7aeCd53cCa1";
